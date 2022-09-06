ec758bd890176e7b44cc27308b9daf76abbe773f
Hildegunn80 <94168921+Hildegunn80@users.noreply.github.com>
2022-09-06 19:53:31 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,42 @@ const cats = [
     }
 ];
 
+
+//Question 1//
+const cat = {
+   complain:function() {
+   console.log("Meow!");
+  }}
+
+cat.complain()
+
+
+//Question 2//
+
+const heading =document.querySelector("h3");
+  heading.textContent="updated heading";
+
+//Question 3//
+heading.style.fontSize="2em";
+
+//Question 4//
+
+const addSubHeading = document.querySelector("h3");
+ for(let i=0;i<addSubHeading.lenght;i++){
+    addSubHeading[i].classList.add("subheading");
+ }
+
+ //Question 5//
+
+ const paragraphs = document.querySelectorAll("p");
+    paragraphs.style.color="red";
+
+
+
+
+
+
+
+
+
+
