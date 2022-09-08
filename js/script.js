3404a7672644fe338e75bd5f4c460b6ef4cf562b
Hildegunn80 <94168921+Hildegunn80@users.noreply.github.com>
2022-09-08 20:03:21 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -41,25 +41,29 @@ for(let i=0; i<paragraphs.length; i++) {
 }
 
 
-/*
-Question 6:
-Select the div with a class of results, assign it to a variable called resultsContainer and set its inner HTML to be <p>New paragraph</p> and its background colour to be yellow.
+//Question 6
+//Select the div with a class of results, assign it to a variable called resultsContainer 
+//and set its inner HTML to be <p>New paragraph</p> and its background colour to be yellow.
 
-Question 7:
-Create a function that has one parameter called list.
 
-Inside the function, loop through the list parameter and console log the name property in each object.
+const resultsContainer = document.querySelector(".results");
+resultsContainer.innerHTML="<p>New paragraph</p>";
 
-Call the function and pass in the cats variable in the script.js file in the repo.
 
-Question 8:
-Create a function called createCats. The function will have one parameter called cats.
-Inside the function loop through the value passed in as cats and create HTML for each object in the array.
-Wrap each item in a div, each name property in an h5 tag and each age property in a p tag.
-If the age property is missing, it should display “Age unknown” instead.
+//Question 7:
+//Create a function that has one parameter called list.
 
-Return the HTML from the function.
-Call the function and pass in the cats array as the argument.
+//Inside the function, loop through the list parameter and console log the name property in each object.
 
-Assign the return value of the function to the innerHTML property of the element on the HTML page with a class of cat-container.
-*/
+//Call the function and pass in the cats variable in the script.js file in the repo.
+
+//Question 8:
+//Create a function called createCats. The function will have one parameter called cats.
+//Inside the function loop through the value passed in as cats and create HTML for each object in the array.
+//Wrap each item in a div, each name property in an h5 tag and each age property in a p tag.
+//If the age property is missing, it should display “Age unknown” instead.
+
+//Return the HTML from the function.
+//Call the function and pass in the cats array as the argument.
+
+//Assign the return value of the function to the innerHTML property of the element on the HTML page with a class of cat-container.
