e7c468c937541eb5b015fa34719c8f3f0ca3a00c
Hildegunn80 <94168921+Hildegunn80@users.noreply.github.com>
2022-09-08 21:36:17 +0200

    q7 done

--- js/script.js
+++ js/script.js
@@ -34,41 +34,46 @@ heading.style.fontSize="2em";
 heading.classList.add("subheading");
 
 
-//Question 5
+// Question 5
 const paragraphs = document.querySelectorAll("p");
 for(let i=0; i<paragraphs.length; i++) {
     paragraphs[i].style.color = "red";
 }
 
 
-//Question 6
-//Select the div with a class of results, assign it to a variable called resultsContainer 
-//and set its inner HTML to be <p>New paragraph</p> and its background colour to be yellow.
-
-
+// Question 6
+// Select the div with a class of results, assign it to a variable called resultsContainer 
+// and set its inner HTML to be <p>New paragraph</p> and its background colour to be yellow.
 const resultsContainer = document.querySelector(".results");
 resultsContainer.innerHTML="<p>New paragraph</p>";
+resultsContainer.style.background = "Yellow";
 
-
-//Question 7:
+// Question 7:
 //Create a function that has one parameter called list.
-
 //Inside the function, loop through the list parameter and console log the name property in each object.
-
 //Call the function and pass in the cats variable in the script.js file in the repo.
+function quest7(list) {
+    for (let i=0;i<list.length;i++) {
+        console.log("Name: " + list[i].name );
+    }
+}
+quest7(cats);
 
 //Question 8:
 //Create a function called createCats. The function will have one parameter called cats.
 
-createCats.forEach(cats)=>{
-    cats.innerHTML= <div>
-                        <h5>{cats.name}</h5>
-                        <p>{cats.age}</p>
-                      </div>;
+/*
+function createCats(cats) {
+    for (let i=0;i<cats.length;i++) {
+        cats[i].innerHTML= "<div><h5>{"cats.name}</h5><p>{cats.age}</p></div>";
+    }
 }
-createCats()
+
+createCats(cats);
+*/
 
 //Inside the function loop through the value passed in as cats and create HTML for each object in the array.
+
 //Wrap each item in a div, each name property in an h5 tag and each age property in a p tag.
 //If the age property is missing, it should display “Age unknown” instead.
 
